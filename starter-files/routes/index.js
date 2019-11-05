1730402a0325cbbc58d74f0de638d84297558915
Adam Wu <wutianhao93923@gmail.com>
2019-11-05 22:45:56 +0800

    add new router for request and get info from the request url

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -10,6 +10,12 @@ router.get('/', (req, res) => {
     title: 'Tianhao Wu'
   });
 
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
+
   const adam = { name: 'Adam', age: 100, cool: true };
   // res.json(adam);
   res.json(req.query);
