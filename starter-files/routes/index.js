a6d8aca8aed1e7a612d38ca734b742f9b4796ab8
Adam Wu <wutianhao93923@gmail.com>
2019-11-05 22:51:32 +0800

    use a controller function to render a .pug file

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.render('hello', {
-    name: 'Adam',
-    number: 2048,
-    Id: req.query.Id,
-    title: 'Tianhao Wu'
-  });
-
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
