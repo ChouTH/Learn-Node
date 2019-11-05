0e5904458691c548f36a64db5475e22510de10d4
Adam Wu <wutianhao93923@gmail.com>
2019-11-05 22:40:56 +0800

    use res.json to return json obj

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+
+  const adam = { name: 'Adam', age: 100, cool: true };
+  res.json(adam);
 });
 
 module.exports = router;
