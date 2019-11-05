c9eeec0f215dc937496bbb52385c2bcfe36a6ae3
Adam Wu <wutianhao93923@gmail.com>
2019-11-05 22:44:20 +0800

    use res.render to render a .pug file

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  // res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'Adam',
+    number: 2048,
+    Id: req.query.Id,
+    title: 'Tianhao Wu'
+  });
 
   const adam = { name: 'Adam', age: 100, cool: true };
   // res.json(adam);
